ffd76108015abea9eb6cf13b891d844634287f01
Sebastian Belalcazar <sebasbelmosxd@gmail.com>
2023-08-15 22:28:24 -0500

    Welcome

---
+++ exer_2.py
@@ -0,0 +1,34 @@
+#num order
+#n_ord 
+
+#Nombre del Remitente.
+
+#Dirección Remitente.
+
+#Teléfono del Remitente
+
+#Nombre del Destinatario
+
+#Dirección del Destinatario
+
+#Teléfono del Destinatario
+
+#Fecha del envío.
+
+#Ciudad de origen.
+
+#Ciudad de destino.
+
+#Tipo de envío (normal, rápido o extra rápido).
+
+#peso?
+
+#menu origin
+while True:
+    opcion_origin = input("Digite una opcion (1) Cali \n (2) Bogotá o Medellín \n (3) Cartagena, Santa Marta, Barranquilla \n (4) Otra ciudad \n (0) Salir: " )
+    if opcion_origin == "1":
+        #try:
+        None
+        #except:
+    else:
+        break
