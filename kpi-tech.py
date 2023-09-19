66afc27e6c4a40de0cb39b959c3e944410ecaa8f
Sebastian Belalcazar <sebasbelmosxd@gmail.com>
2023-09-19 13:57:23 -0500

    pd

---
+++ kpi-tech.py
@@ -0,0 +1,7 @@
+"Welcome to KPI TECH, this is our repository, follow our project"
+
+import pandas as pd
+
+salida = pd.read_json("C:/Users/sebas/OneDrive/Documentos/GitHub/KPI-Tech/salida.json")
+
+print(salida)
