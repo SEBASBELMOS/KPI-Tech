12bd380d9138aaa26e7a9ce3d6f592c7028dd39d
Sebastian Belalcazar <sebasbelmosxd@gmail.com>
2023-10-02 22:55:25 -0500

    KPIs - Alpha

--- kpi-tech.py
+++ kpi-tech.py
@@ -2,6 +2,9 @@
 
 import pandas as pd
 import json
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Lectura del JSON
 with open("C:/Users/sebas/OneDrive/Documentos/GitHub/KPI-Tech/salida.json", "r") as file:
@@ -32,3 +35,21 @@ sorted_data_desc = data_2019_to_2023.sort_values(by='fecha_creacion', ascending=
 sorted_loc_data = sorted_data_desc.loc[:, ['detalle', "fecha_creacion"]]
 print(" \nsorted/date_data_descending")
 print(sorted_loc_data)
+
+
+##DATA VISUALIZATION##
+
+# Calcular Tiempo Promedio de Primera Respuesta (TAPR)
+df['solucion']['fecha_inicio'] = pd.to_datetime(df['solucion']['fecha_inicio'])
+df['tiempo_primera_respuesta'] = (df['solucion']['fecha_inicio'] - df['fecha_creacion']).dt.total_seconds() / 60  # en minutos
+average_first_response_time = df['tiempo_primera_respuesta'].mean()
+
+# Crear un gráfico de barras
+kpis = ['Tiempo Promedio de Resolución (TAR)', 'Tiempo Promedio de Primera Respuesta (TAPR)']
+valores = [average_resolution_time, average_first_response_time]
+
+plt.figure(figsize=(10, 6))
+plt.barh(kpis, valores, color=['blue', 'green'])
+plt.xlabel('Minutos')
+plt.title('KPIs de Tiempo Promedio')
+plt.show()
